77903059eeaddc83c69d2a11e0bac89e3018109f
Igor Kanshyn <igor.kanshyn@gmail.com>
2021-03-10 22:08:33 -0500

    initial commit

---
+++ src/main/java/com/grenader/sample/circlecitest/CircleCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.grenader.sample.circlecitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiTestApplication.class, args);
+    }
+
+}
